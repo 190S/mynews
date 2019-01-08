63613b56157062488091513d8fef8039cd29736c
vagrant <vagrant@vagrant.vm>
2019-01-08 13:06:12 +0000

    PHP12_rebuild

--- mynews/resources/views/layouts/profile.blade.php
+++ mynews/resources/views/layouts/profile.blade.php
@@ -24,9 +24,8 @@
         {{-- Laravel標準で用意されているCSSを読み込みます --}}
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+
         {{-- この章の後半で作成するCSSを読み込みます --}}
-        {{-- 次の1行を削除 --}}
-        <!-- <link href="{{ asset('css/admin.css') }}" rel="stylesheet"> -->
         {{-- 次の1行を追記 --}}
         <link href="{{ asset('css/profile.css') }}" rel="stylesheet">
     </head>
